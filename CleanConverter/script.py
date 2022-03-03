6a4f11f33e1a1590d025d8d8cac77d63c385c0fa
Icerath <dorje.gilfillan@gmail.com>
2022-03-04 00:21:27 +0200

    Fixes a few bugs, improved config and readme

--- CleanConverter/script.py
+++ CleanConverter/script.py
@@ -1,4 +1,3 @@
-#imports
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,14 +16,14 @@ base_url = "https://mariadb.com"
 
 #functions
 def main():
-    if not config["read_off_output"]:
+    #get html
+    if config["new_html"]:
         html = make_html()
-    else:
-        path = os.path.join("output", config["output_html"])
-        with open(path, "r", encoding = "utf-8") as file:
-            html = file.read()
-    if config["write_to_html"]:
         write_to_html(html)
+    else:
+        html = read_html()
+    
+    #write to pdf
     if config["write_to_pdf"]:
         write_to_pdf(html)
 def make_html():
@@ -39,15 +38,13 @@ def make_html():
             continue
         elif include == 2:
             add = row["Header"]
-            full_html += f'\n<h1 class="col-md-8">{add}</h1>\n'
+            full_html += f'\n<h1 class="col-md-8" style="margin-top:0px">{add}</h1>\n'
         elif include == 3:
             url = row["URL"]
             name = _get_name(url)
             add = row["Header"]
-            full_html += f'\n<h1 class="col-md-1"><a href="#{name}">{add}</a></h1>\n'
+            full_html += f'\n<h1 class="col-md-8"><a href="#{name}">{add}</a></h1>\n'
         elif include == 1:
-            if stripped > 0 and stripped % 50 == 0:
-                print(f"stripped {stripped}")
             time_since = time.perf_counter() - last_requested
             if config["min_sleep_time"] - time_since > 0:
                 print("\nsleeping for " + str(config["min_sleep_time"] - time_since))
@@ -59,10 +56,10 @@ def make_html():
             filename = name + ".html"
             if filename not in existing_files or config["request_existing_files"]:
                 print(f"\nrequesting {name}")
-                _request_and_write(url, filename)
                 last_requested = time.perf_counter()
-
-            html = _get_html_file(filename, existing_files)
+                _request_and_write(url, filename)
+            #read full html
+            html = _get_html_file(filename)
 
 
             html = _strip(html, name, row)
@@ -71,7 +68,12 @@ def make_html():
         #to keep sameline 
         sys.stdout.write(f"\rrun through {index + 1} rows")
         sys.stdout.flush()
-    sys.stdout.write("\n")#to clear for next line
+
+    #final fixes
+    print("\nfinal fixes")
+    if config["flatten_internal_contents"]:
+        full_html = edit_contents(full_html)
+    sys.stdout.write("\n")#to clear for next line   
     boiler, plate = _get_boilerplate()
     return boiler + full_html + plate
 
@@ -107,11 +109,13 @@ def _strip(html, name, row):
     return text
 
 
-def _get_html_file(filename, existing_files):
+def _get_html_file(filename):
     path = os.path.join("html", filename)
     with open(path, "r", encoding="utf-8") as file:
         contents = file.read()
     
+
+    #set css link if not yet set
     if config["css-link"] == "":
         match = re.search(r'[^"]+\.css', contents)
         if match != None:
@@ -130,19 +134,19 @@ def _request_and_write(url, filename):
 def _convert_links(html, unique_id):
     #make absolute
     html = html.replace('href="/kb/en', 'href="' + base_url + "/kb/en")
+    html = html.replace('src="/kb/en', 'src="' + base_url + "/kb/en")
     #make id's unique
     find_pattern = r'"#[\w-]+"' #changing this means changes string = string[1:]
     hash_tags = re.findall(find_pattern, html)
     for string in hash_tags:
         string = string[2:][:-1]
-        find = f'"{string}"'
-        replacement = f'"{unique_id}{string}"'
+        find = f'id="{string}"'
+        replacement = f'id="{unique_id}{string}"'
         html = html.replace(find, replacement)
 
-        find = f'"#{string}"'
-        replacement = f'"#{unique_id}{string}"'
+        find = f'href="#{string}"'
+        replacement = f'href="#{unique_id}{string}"'
         html = html.replace(find, replacement)
-
     html = _make_internal(html)
     html = _make_single_internals(html)
 
@@ -154,20 +158,30 @@ def _make_internal(html):
         if row["URL"] != "":
             url = row["URL"]
             name = _get_name(url)
-            html = html.replace(url, "#" + name)
+            #image_replacement = url.strip("https")
+            #tml = html.replace(url + r"\+", image_replacement + r"\+")
+            html = html.replace('href="'+ url, 'href="#' + name)
     return html
 
 def _make_single_internals(html):
     pattern = r'(href ?= ?")(#[\w-]+)#([\w-]+)'
     html = re.sub(pattern, r"\1\2\3", html)
     return html
+
+
+def edit_contents(html):
+    find = '<div class="table_of_contents'
+    replace = '<div style="float: none;" class="table_of_contents'
+                    
+    html = html.replace(find, replace)
+    return html
+
 def _get_rows():
     with open(config["input_csv"]) as file:
         contents = list(csv.DictReader(file))
 
     if config["number_of_rows"] > 0:
         contents = contents[:config["number_of_rows"]]
-
     return contents
 
 def _get_name(url):
@@ -205,39 +219,22 @@ def _add_css(string):
     return string
 
 def write_to_pdf(html):
-    """WORK IN PROGRESS"""
-
-    #check to see if wkhtmltopdf is in the directory
-    for filename in os.listdir():
-        if filename.find("wkhtmltopdf") != -1: break
-    else: raise OSError("wkhtmltopdf is not in the correct directory")
-
-
     import pdfkit
-    path_to_exe = "wkhtmltopdf.exe"
-    pdf_config = pdfkit.configuration(wkhtmltopdf=path_to_exe)
-    options = {
-        'margin-bottom': '1cm', 
-        'margin-top': '1cm',
-        'footer-line': '',
-        'page-size': 'Letter',
-        'encoding': "UTF-8",
-        'footer-right': '[page] /[topage]',
-     }
-
     print("\nmaking_pdf")
 
+    pdf_config = pdfkit.configuration(wkhtmltopdf = config["path_to_exe"])
     pdf_file = config["output_pdf"]
-
     path = os.path.join("output", pdf_file)
+
+    #for easy call
+    pdfcall = lambda html, path, wk_config, pdf_options: pdfkit.from_string(html, path, configuration = wk_config, options = pdf_options)
+    args = html, path, pdf_config, config["options"]
     if config["catch-OSError"]:
         try:
-            pdfkit.from_string(html, path, configuration = pdf_config, options = options)
-        except OSError:
-            pass
-    else:
-        pdfkit.from_string(html, path, configuration = pdf_config, options = options)
-    
+            pdfcall(*args)
+        except OSError: pass
+    else: pdfcall(*args)
+
     print(f"pdf written to {path}")
 
 def write_to_html(html):
@@ -246,5 +243,9 @@ def write_to_html(html):
     with open(path, "w", encoding = "utf-8") as file:
         file.write(html)
 
-
+def read_html():
+    path = os.path.join("output", config["output_html"])
+    with open(path, "r", encoding = "utf-8") as file:
+        html = file.read()
+    return html
 main()
