366ae6dff74f47e49cc34269cec3c131d0be36d8
Icerath <dorje.gilfillan@gmail.com>
2022-03-16 20:59:15 +0200

    Added font weight scaling for the toc however the font currently does not support that

--- CleanConverter/used/generate_contents.py
+++ CleanConverter/used/generate_contents.py
@@ -21,7 +21,7 @@ class content_counter():
         self.header = f'\n<h1 class="col-md-8" style="{self.header_style}">{header_txt}</h1>\n'
         self.a_style ="color: #333333; text-decoration: none; font-size: {rep}px;"
     
-    def add_content(self, header, name, depth, max_depth):
+    def add_content(self, header, name, depth, no_children, max_children):
         """nests li elements based on depth and adds that element to content"""
         if depth > self.prev_depth:
             self.content += f'<ol style="margin-left: {self.margin_left}em;">'
@@ -36,38 +36,46 @@ class content_counter():
         text = f"{header}"
         if depth == 1:
             text = "Chapter " + text
-            a_style += " font-weight: bold;"
-        self.content += f'<li style="{self.li_style}"><a href="#{name}" style="{a_style}">{text}</a></li>\n'
+        boldness = int(700 * (no_children/max_children))
+        a_style += f" font-weight: {boldness};"
+        self.content += f'<li style="{self.li_style}"><a href="#{name}" style="{a_style}">{text + " " + str(boldness)}</a></li>\n'
     
     def get_contents(self, depth):
         "finishes html and return contents"
         return self.header + self.content + ("</ol>"*depth)
     
 def create_main_contents(ids):
-    #creating counter objects
-    #       -- debug for indents --
-    #   print(2.5-BODY["table_indent"])
-    #   print(2.5-BODY["chapter_indent"])
-
     main = content_counter(
         header_txt = HEADER["table"], body_font = BODY["table_font_size"], margin_left=BODY["table_indent"]-2.5, font_scaling = BODY["table_scaling"])
     chapters = content_counter(
         header_txt = HEADER["chapters"], body_font = BODY["chapter_font_size"], margin_left=BODY["chapter_indent"]-2.5, font_scaling = BODY["chapter_scaling"])
+    
     #lambda function to get depth int from Fstr
     get_depth = lambda depth_str: depth_str.count(".") + 1 if depth_str != "" else 0
     #main loop going through each id and giving the information to the counter
-    max_depth = find_max_depth(ids, get_depth)
-    for header, name, depth_str in ids:
-        depth = get_depth(depth_str)
-        #adding to main counter
-        #adding to chapter counter if depth is in a certain range
-        main.add_content(header, name, depth, max_depth)
+    arr = [(header, name, get_depth(depth_str)) for header, name, depth_str in ids]
+    max_children = max([depth_of_children(index, arr) for index, _ in enumerate(arr)])
+    for index, (header, name, depth) in enumerate(arr):
+        main.add_content(header, name, depth, depth_of_children(index, arr), max_children)
         range_of_depths = [1, 2]
-        if depth in range_of_depths: chapters.add_content(header, name, depth, max(range_of_depths))
+        if depth in range_of_depths:
+            chapters.add_content(header, name, depth, depth_of_children(index, arr), max_children)
     
     #print(content_counter.scaling_levels)
     return chapters.get_contents(depth) + new_page + main.get_contents(depth) + new_page
 
+def depth_of_children(sindex, arr):
+    count = 0
+    prev_depth = 0
+    base = arr[sindex][2]
+    for index, (_, _, depth) in enumerate(arr[sindex:]):
+        if depth < prev_depth:
+            break
+        elif depth > prev_depth:
+            count += 1#depth - prev_depth
+            prev_depth = depth
+
+    return count
 def find_max_depth(ids, depth_func):
     """highly inefficient mess"""
     highest_depth = 0
